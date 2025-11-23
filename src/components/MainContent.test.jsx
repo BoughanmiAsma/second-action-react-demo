b971daa001e5d2f4a3798c51bfdda06c88fd3bf4
BoughanmiAsma <asmaboughanmi2020@gmail.com>
2025-11-23 17:15:54 +0100

    Revert "Test cassé volontairement"
    This reverts commit d9414c1d5774ab5d6803d0cfe13226952a83c93e.

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -8,7 +8,12 @@ describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
 
-    expect(screen.getByRole('button')).not.toBeInTheDocument();
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  it('help area should not be visible initially', () => {
+    render(<MainContent />);
+    expect(screen.queryByTestId('help-area')).not.toBeInTheDocument();
   });
   
 
